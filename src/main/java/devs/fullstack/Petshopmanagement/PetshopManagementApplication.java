24385d541741267732e82bb4b66bc03e7d5c114e
Dikendev <diken.dev@gmail.com>
2023-02-22 14:56:57 -0300

    Initial commit

---
+++ src/main/java/devs/fullstack/Petshopmanagement/PetshopManagementApplication.java
@@ -0,0 +1,13 @@
+package devs.fullstack.Petshopmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshopManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devs/fullstack/Petshopmanagement/PetshopManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package devs.fullstack.Petshopmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
