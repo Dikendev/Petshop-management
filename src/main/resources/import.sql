101b625f8e1053a0793b2c27ff1d0381d0621c53
Dikendev <diken.dev@gmail.com>
2023-02-23 23:59:11 -0300

    Loading initial data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_department(department_type) VALUES ('BEAUTY');
+INSERT INTO tb_department(department_type) VALUES ('EMERGENCY');
+
+
+INSERT INTO tb_animals(department_id, name, species, breed, weight, hair_type, treat_type) VALUES (1, 'Joy', 'dog', 'Lhasa apso', '1mt', 'liso curto', 'TREAT');
+INSERT INTO tb_animals(department_id, name, species, breed, weight, hair_type, treat_type) VALUES (1, 'Chato', 'cat', 'indefinido', '2mt', 'liso longo', 'MEAL');
+INSERT INTO tb_animals(department_id, name, species, breed, weight, hair_type, treat_type) VALUES (1, 'Feijao', 'dog', 'caro', '3mt', 'liso medio', 'TREAT');
+INSERT INTO tb_animals(department_id, name, species, breed, weight, hair_type, treat_type) VALUES (2, 'Pudim', 'cat', 'vira lata', '4mt', 'liso longo', 'SNACK');
