1afe0997229b7f45747b00cb7c6274f29a0e52cf
Dikendev <diken.dev@gmail.com>
2023-02-24 23:57:53 -0300

    implemented findById method and transactional annotation

--- src/main/java/devs/fullstack/Petshopmanagement/services/AnimalService.java
+++ src/main/java/devs/fullstack/Petshopmanagement/services/AnimalService.java
@@ -1,20 +1,34 @@
 package devs.fullstack.Petshopmanagement.services;
 
 import devs.fullstack.Petshopmanagement.repositories.AnimalRepository;
-import devs.fullstack.Petshopmanagement.entities.Animal;
+import devs.fullstack.Petshopmanagement.models.AnimalModel;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AnimalService {
     @Autowired
     private AnimalRepository animalRepository;
-    public List<Animal> findAll() {
+
+    @Transactional
+    public AnimalModel addAnimal(AnimalModel animalModel) {
+        return animalRepository.save(animalModel);
+    }
+
+    @Transactional
+    public void deleteAnimal(AnimalModel animalModel) {
+        animalRepository.delete(animalModel);
+    }
+
+    public List<AnimalModel> findAll() {
         return animalRepository.findAll();
     }
-    public Animal addAnimal(Animal animal) {
-        return animalRepository.save(animal);
+
+    public Optional<AnimalModel> findById(Long id){
+        return animalRepository.findById(id);
     }
 }
--- src/main/java/devs/fullstack/Petshopmanagement/services/DepartmentService.java
+++ src/main/java/devs/fullstack/Petshopmanagement/services/DepartmentService.java
@@ -1,12 +1,10 @@
 package devs.fullstack.Petshopmanagement.services;
 
 
-import devs.fullstack.Petshopmanagement.entities.Department;
+import devs.fullstack.Petshopmanagement.models.DepartmentModel;
 import devs.fullstack.Petshopmanagement.repositories.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -16,11 +14,11 @@ public class DepartmentService {
     @Autowired
     private DepartmentRepository departmentRepository;
 
-    public List<Department> findAll() {
+    public List<DepartmentModel> findAll() {
         return departmentRepository.findAll();
     }
 
-    public Department addDepartment(Department department) {
+    public DepartmentModel addDepartment(DepartmentModel department) {
         return departmentRepository.save(department);
     }
 }
