408bdfabedfb687b1ff417e9801eea154f805be6
Dikendev <diken.dev@gmail.com>
2023-02-24 23:58:40 -0300

    implemented findById method

--- src/main/java/devs/fullstack/Petshopmanagement/controllers/AnimalController.java
+++ src/main/java/devs/fullstack/Petshopmanagement/controllers/AnimalController.java
@@ -1,15 +1,17 @@
 package devs.fullstack.Petshopmanagement.controllers;
 
-import devs.fullstack.Petshopmanagement.repositories.AnimalRepository;
-import devs.fullstack.Petshopmanagement.entities.Animal;
+import devs.fullstack.Petshopmanagement.models.AnimalModel;
 import devs.fullstack.Petshopmanagement.services.AnimalService;
+import jakarta.persistence.Id;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.yaml.snakeyaml.events.Event;
 
 import java.util.List;
+import java.util.Optional;
 
 @Data
 @RestController
@@ -20,13 +22,24 @@ public class AnimalController {
     private AnimalService animalService;
 
     @GetMapping()
-    public ResponseEntity<List<Animal>> getAllAnimals() {
-        List<Animal> animals = animalService.findAll();
-        return new ResponseEntity<>(animals, HttpStatus.OK);
+    public ResponseEntity<List<AnimalModel>> getAllAnimals() {
+        List<AnimalModel> animalModels = animalService.findAll();
+        return new ResponseEntity<>(animalModels, HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Animal> addAnimal(@RequestBody Animal animal) {
-       return ResponseEntity.status(HttpStatus.CREATED).body(animalService.addAnimal(animal));
+    public ResponseEntity<AnimalModel> addAnimal(@RequestBody AnimalModel animalModel) {
+       return ResponseEntity.status(HttpStatus.CREATED).body(animalService.addAnimal(animalModel));
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Object> findById(@PathVariable Long id) {
+        Optional<AnimalModel> optionalAnimalModel = animalService.findById(id);
+        if (optionalAnimalModel.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("no animals with id" + id);
+        } else {
+            return ResponseEntity.ok().body(optionalAnimalModel.get());
+        }
+    }
+
 }
