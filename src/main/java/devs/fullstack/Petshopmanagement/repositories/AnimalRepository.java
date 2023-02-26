8233197cbec0a07a0ee74fe1e58fcb2f07da4468
Dikendev <diken.dev@gmail.com>
2023-02-26 15:14:44 -0300

    fixed repositories imports

--- src/main/java/devs/fullstack/Petshopmanagement/repositories/AnimalRepository.java
+++ src/main/java/devs/fullstack/Petshopmanagement/repositories/AnimalRepository.java
@@ -1,11 +1,6 @@
 package devs.fullstack.Petshopmanagement.repositories;
-
 import devs.fullstack.Petshopmanagement.models.AnimalModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface AnimalRepository extends JpaRepository<AnimalModel, Long> {
-    Optional<List<AnimalModel>> findByName(String name);
 }
--- src/main/java/devs/fullstack/Petshopmanagement/repositories/DepartmentRepository.java
+++ src/main/java/devs/fullstack/Petshopmanagement/repositories/DepartmentRepository.java
@@ -1,5 +1,4 @@
 package devs.fullstack.Petshopmanagement.repositories;
-
 import devs.fullstack.Petshopmanagement.models.DepartmentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
