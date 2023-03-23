ad1b820eae1e082e72de664b8f28f7476cb02c54
Dikendev <diken.dev@gmail.com>
2023-03-23 08:40:03 -0300

    fix table animal

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,7 +2,7 @@ INSERT INTO tb_department(department_type) VALUES ('BEAUTY');
 INSERT INTO tb_department(department_type) VALUES ('EMERGENCY');
 
 
-INSERT INTO tb_animals(department_id, name, species, breed, weight, hair_type, treat_type) VALUES (1, 'Joy', 'dog', 'Lhasa apso', '1mt', 'liso curto', 'TREAT');
-INSERT INTO tb_animals(department_id, name, species, breed, weight, hair_type, treat_type) VALUES (1, 'Panda', 'cat', 'viralata', '2mt', 'liso longo', 'MEAL');
-INSERT INTO tb_animals(department_id, name, species, breed, weight, hair_type, treat_type) VALUES (1, 'Feijao', 'dog', 'poodle', '3mt', 'liso medio', 'TREAT');
-INSERT INTO tb_animals(department_id, name, species, breed, weight, hair_type, treat_type) VALUES (2, 'Pudim', 'cat', 'vira lata', '4mt', 'liso longo', 'SNACK');
+INSERT INTO tb_animal(department_id, name, species, breed, weight, hair_type, treat_type) VALUES (1, 'Joy', 'dog', 'Lhasa apso', '1mt', 'liso curto', 'TREAT');
+INSERT INTO tb_animal(department_id, name, species, breed, weight, hair_type, treat_type) VALUES (1, 'Panda', 'cat', 'viralata', '2mt', 'liso longo', 'MEAL');
+INSERT INTO tb_animal(department_id, name, species, breed, weight, hair_type, treat_type) VALUES (1, 'Feijao', 'dog', 'poodle', '3mt', 'liso medio', 'TREAT');
+INSERT INTO tb_animal(department_id, name, species, breed, weight, hair_type, treat_type) VALUES (2, 'Pudim', 'cat', 'vira lata', '4mt', 'liso longo', 'SNACK');
