d8226d17b4ce14476503760cc70469e0a503f2e8
Dikendev <diken.dev@gmail.com>
2023-05-08 23:44:37 -0300

    import refactored

--- src/main/java/devs/fullstack/Petshopmanagement/repositories/DepartmentRepository.java
+++ src/main/java/devs/fullstack/Petshopmanagement/repositories/DepartmentRepository.java
@@ -1,5 +1,5 @@
 package devs.fullstack.Petshopmanagement.repositories;
-import devs.fullstack.Petshopmanagement.models.Department;
+import devs.fullstack.Petshopmanagement.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> { }
